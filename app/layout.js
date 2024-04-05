3d548f2afa3e5d81979e065c522afa5d98c6c8f0
James Eldridge <james.eldridge@mail.com>
2024-04-05 11:48:24 -0500

    move drawer html to RootLayout, wrap navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./_components/navbar";
+import NavLinks from "./_components/navbar/NavLinks";
+import MobileNavLinks from "./_components/navbar/MobileNavLinks";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +15,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-          {children}
+        <div className="drawer drawer-end">
+          <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col">
+            <Navbar />
+            <div className="flex min-h-screen flex-col items-center justify-between p-24">
+              {children}
+            </div>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 space-y-4">
+              <MobileNavLinks />
+            </ul>
+          </div>
         </div>
       </body>
     </html>
