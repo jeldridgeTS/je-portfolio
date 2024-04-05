0efe01797fbaf08b8f2dc5a3c05fcea35b979d3b
James Eldridge <jeldridge@turbosquid.com>
2024-04-05 15:20:33 -0500

    add project cards and list component, basic structure and styling

---
+++ app/_components/projectCards/index.jsx
@@ -0,0 +1,11 @@
+import ProjectCardList from "./ProjectCardList";
+
+const ProjectCards = () => {
+  return (
+    <div>
+      <ProjectCardList />
+    </div>
+  );
+};
+
+export default ProjectCards;
