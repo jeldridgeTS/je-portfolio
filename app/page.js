476c8af6505ca6535614d3883228c475db61b708
James Eldridge <jeldridge@turbosquid.com>
2024-04-22 11:23:09 -0500

    setup base structure, add temp cards for landing page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,28 @@
 "use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import LandingPageCard from "@components/landingPage/LandingPageCard";
 
 export default function Home() {
-  const [score, setScore] = useState(0);
-  const increaseScore = () => setScore(score + 1);
-
   return (
-    <div>
-      <p>Your score is {score}</p>
-      <button onClick={increaseScore}>+</button>
+    <div className="flex justify-center">
+      <div>
+        <div className="profileContainer">
+          <Image
+            className="flex-none"
+            alt="profile pic"
+            src="/profPic.webp"
+            width={360}
+            height={360}
+          />
+          <div className="text-center py-10">Stuff about me blah blah blah</div>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-4">
+          <LandingPageCard />
+          <LandingPageCard />
+          <LandingPageCard />
+        </div>
+      </div>
     </div>
   );
 }
