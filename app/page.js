c3da153140f20ae8f454260f0afa934ab87bd6af
James Eldridge <jeldridge@turbosquid.com>
2024-04-30 18:43:20 -0500

    update landing page styling/structure

--- app/page.js
+++ app/page.js
@@ -10,34 +10,32 @@ const rubik400 = Rubik({ weight: "400", variable: true, subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="w-full">
-        <div className="profilePic">
-          <Image
-            className="flex-none border-secondary border-2 rounded-full"
-            alt="profile pic"
-            src="/profPic.webp"
-            width={320}
-            height={320}
-          />
-        </div>
-        <div className="flex flex-col items-center justify-center lg:flex-row gap-4 py-14">
-          <LandingXpCard />
-          <LandingProjectsCard />
-          <LandingAboutCard />
-        </div>
-        <div className="text-center">
-          <div
-            className={`${rubik400.className} text-neutral text-center text-xl`}
-          >
-            James Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </div>
+    <div className="w-full">
+      <div className="profilePic">
+        <Image
+          className="flex-none border-secondary border-2 rounded-full"
+          alt="profile pic"
+          src="/profPic.webp"
+          width={320}
+          height={320}
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center lg:flex-row gap-4 py-14">
+        <LandingXpCard />
+        <LandingProjectsCard />
+        <LandingAboutCard />
+      </div>
+      <div className="text-center">
+        <div
+          className={`${rubik400.className} text-neutral text-center text-xl`}
+        >
+          James Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
     </div>
